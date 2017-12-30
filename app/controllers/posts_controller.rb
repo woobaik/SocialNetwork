d9edf49b910e3376aeccc18cc3bc807529789c01
Joungwoo <joungwoo.baik@gmail.com>
2017-12-30 12:59:20 -0800

    Modified white-space preline to posts.scss, DRY and added before action  for posts_controller, and added modify button for index.html

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
+  before_action :check_ownership, only: [:edit, :update, :destroy]
+
   def index
     @posts = Post.all.order('created_at desc')
     @posts_count = current_user.posts.length
@@ -16,5 +18,28 @@ class PostsController < ApplicationController
     else
       redirect_to new_post_url
     end
+  end
+
+  def edit
+  end
+
+  def update
+    @post.content = params[:content]
+    if @post.save
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_url
+  end
+
+  private
+  def check_ownership
+    @post = Post.find_by(id: params[:id])
+    redirect_to root_path if @post.user_id != current_user.id
   end
 end
