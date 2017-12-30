4e0dabe80409cb1fd2db982abb29393e6198c9b4
Joungwoo <joungwoo.baik@gmail.com>
2017-12-30 13:18:10 -0800

    Defined Like model. Created N-N 'Like' relation between Post and User through Like model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts
 
+  has_many :likes
+  has_many :liked_posts, through: :likes, source: :post
+
 end
