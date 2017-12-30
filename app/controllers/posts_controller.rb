8df9f275ec4e1ccb5c77c5725f889fc7bd211ef7
Joungwoo <joungwoo.baik@gmail.com>
2017-12-29 20:12:16 -0800

    Implemented Materialize to post_view page, and added action for new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,20 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
   def index
+    @posts = Post.all.order('created_at desc')
+    @posts_count = current_user.posts.length
+  end
+
+  def new
+
+  end
+
+  def create
+    new_post = Post.new(user_id: current_user.id, content: params[:content])
+    if new_post.save
+      redirect_to root_url
+    else
+      redirect_to new_post_url
+    end
   end
 end
